47f2bf454646dab0dd4f6a55c799767d95c915fd
anujk <anujkaushik1512@gmail.com>
2021-07-03 22:20:19 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:loginn_page/Screens/homepage.dart';
+import 'package:loginn_page/Screens/login_page.dart';
 
 void main() {
   runApp(MyCoolApp());
 }
 
 class MyCoolApp extends StatelessWidget {
-  const MyCoolApp({ Key? key }) : super(key: key);
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("This is my first login page"),
-          ),
-        ),
-      ),
+   //   home: HomePage(),             //ye rout ki tarah kaam kr rha hai
+      routes: {
+        "/" : (context) =>LoginPage(),  // my default yeh page ke liye(intent launher jaisa hota hai yeh)
+          "/loginpage" : (context) => LoginPage()
+          
+      },
     );
+    
   }
 }
 
